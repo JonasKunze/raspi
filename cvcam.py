672915d9ddd30bdf25712a13e428ec7db48b8838
Jonas Kunze <kunze.jonas@gmail.com>
2016-06-22 20:27:08 +0200

    classified cvcam.py

--- cvcam.py
+++ cvcam.py
@@ -8,26 +8,41 @@ import picamera.array
 screen_width = 640
 screen_height = 480
 
-camera = picamera.PiCamera()
-camera.resolution = (screen_width, screen_height)
+class PiCam(object):
+    def __init__(self):
+        self.cam = picamera.PiCamera()
+        self.resolution = (screen_width, screen_height)
+        self.cam.resolution = self.resolution
+        self.video = picamera.array.PiRGBArray(self.cam)
 
-pygame.init()
-pygame.display.set_caption("OpenCV camera stream on Pygame")
+    def __enter__(self):
+        return self
 
-screen = pygame.display.set_mode([screen_width, screen_height])
-video = picamera.array.PiRGBArray(camera)
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.cam.close()
 
-try:
-    for frameBuf in camera.capture_continuous(video, format ="rgb", use_video_port=True):
-        frame = np.rot90(frameBuf.array)        
-        video.truncate(0)
+    def draw_preview(self, frame_buffer):
+        frame = np.rot90(frame_buffer.array)        
+        self.video.seek(0)
         frame = pygame.surfarray.make_surface(frame)
         screen.fill([0,0,0])
         screen.blit(frame, (0,0))
         pygame.display.update()
-        
-        for event in pygame.event.get():
-         if event.type == KEYDOWN:
-          raise KeyboardInterrupt
-except KeyboardInterrupt,SystemExit:
-    pygame.quit()
+
+    def get_stream(self):
+        return self.cam.capture_continuous(self.video, format ="rgb", use_video_port=True, resize=self.cam.resolution)
+
+with PiCam() as cam:
+    pygame.init()
+    pygame.display.set_caption("OpenCV camera stream on Pygame")
+
+    screen = pygame.display.set_mode([screen_width, screen_height])
+
+    try:
+        for frameBuf in cam.get_stream():
+            cam.draw_preview(frameBuf)
+            for event in pygame.event.get():
+                if event.type == KEYDOWN:
+                    raise KeyboardInterrupt
+    except KeyboardInterrupt,SystemExit:
+        pygame.quit()
